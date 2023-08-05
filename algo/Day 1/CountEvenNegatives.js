4e564d3532b935ab049690155f1182ebcedb861b
unknown <khaledguesmi889@gmail.com>
2023-08-05 19:45:42 +0100

    Day 1

---
+++ algo/Day 1/CountEvenNegatives.js
@@ -0,0 +1,32 @@
+/* 
+Given an array of numbers,
+return a count of how many are both even and negative.
+*/
+
+const nums1 = [1, 5, -1, 2, -4, 9, -10, 0, -3, -2];
+const expected1 = 3;
+
+const nums2 = [];
+const expected2 = 0;
+
+const nums3 = [-4, -2, -6];
+const expected3 = 3;
+
+/**
+ * Counts how many numbers are both even and negative.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} nums
+ * @returns {number} The count.
+ */
+function countEvenNegatives(nums) { 
+    CEN=0
+        for(i=0;i<=nums.length-1;i++){
+            if((nums[i]%2==0)&&(nums[i]<0)){
+                CEN=CEN+1
+            }
+        }
+        return CEN 
+}
+var x=countEvenNegatives(nums2);
+console.log(x);
